2dd4fbef5a273fe518a78e47d10f29fb758d1bf7
Greys <razor2911ss@gmail.com>
2026-09-09 13:37:02 +0900

    Add AppState class

---
+++ Rpi_block/src/app_state.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <mutex>
+#include <nlohmann/json.hpp>
+
+class AppState {
+public:
+    void setTemperature(double value);
+    nlohmann::json snapshot() const;
+
+private:
+    mutable std::mutex mutex_;
+    double temperature_c_ = 20.0;
+};
+
+void AppState::setTemperature(double value) {
+    std::lock_guard<std::mutex> lock(mutex_);
+    temperature_c_ = value;
+}
+
+nlohmann::json AppState::snapshot() const {
+    std::lock_guard<std::mutex> lock(mutex_);
+    nlohmann::json j;
+    j["temperature_c"] = temperature_c_;
+    return j;
+}
